91b1a131f4394677eed3293567442cafebf6e730
DenisUstinovMIET <denisustinovmiet@mail.ru>
2022-10-27 11:28:57 +0300

    Дз4

---
+++ DZ4/Program.cs
@@ -0,0 +1,49 @@
+﻿
+/*
+ * Вариант 17
+Одномерные массивы
+В одномерном массиве, состоящем из n-целочисленных элементов, вычислить:
+ количество положительных элементов массива;
+ сумму элементов массива, расположенных после последнего элемента, равного нулю.
+Преобразовать массив таким образом, чтобы сначала располагались все элементы, 
+целая часть которых не превышает единицу, а потом — все остальные.*/
+
+using System;
+
+namespace DZ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, positive_sum = 0, last_zero = 0, last_zero_sum = 0;
+            Console.Write("Введите количество элементов массива: ");
+            while (!int.TryParse(Console.ReadLine(), out n))
+                Console.Write("Введите количество элементов массива в виде натурального числа:");
+
+            Console.WriteLine("Значение начального массива:");
+            int[] Array = new int[n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                Array[i] = rand.Next(-5, 5);
+                Console.Write(Array[i] + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (Array[i] > 0) positive_sum++;
+                if (Array[i] == 0) last_zero = i;
+            }
+
+            for (int i = last_zero; i < n; i++)
+            {
+                last_zero_sum = last_zero_sum + Array[i];
+            }
+
+            Console.WriteLine("Количество положительных элементов массива: " + positive_sum);
+            Console.WriteLine("Сумму элементов массива, расположенных после последнего элемента, равного нулю: " + last_zero_sum);
+        }
+    }
+}
---
+++ DZ42/Program.cs
@@ -0,0 +1,113 @@
+﻿
+/*
+ * Вариант 17
+Двумерные массивы
+Путем перестановки элементов квадратной вещественной матрицы добиться того, 
+чтобы ее максимальный элемент находился в левом верхнем углу, 
+следующий по величине — в позиции (2, 2), следующий по величине — в позиции (3, 3) и т. д., 
+заполнив таким образом всю главную диагональ.
+Найти номер первой из строк, не содержащих ни одного положительного элемента.
+ */
+using System;
+
+namespace DZ42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0, max = 10;
+
+            int n, buf = 0;
+            Console.Write("Введите количество элементов массива: ");
+            while (!int.TryParse(Console.ReadLine(), out n))
+                Console.Write("Введите количество элементов массива в виде натурального числа:");
+
+            Console.WriteLine("Значение начального массива:");
+            int[,] Array = new int[n, n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Array[i, j] = rand.Next(min, max);
+                    Console.Write(Array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int[] Array_index_i = new int[n], Array_index_j = new int[n];
+            int[] Array_max = new int[n];
+            for (int k = 0; k < n; k++)
+            {
+                Array_index_i[k] = k;
+                Array_index_j[k] = k;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if ((Array[i, j] > Array[Array_index_i[k], Array_index_j[k]]) && i != j)
+                        {
+                            Array_index_i[k] = i;
+                            Array_index_j[k] = j;
+                        }
+                    }
+                }
+                if (Array[k, k] < Array[Array_index_i[k], Array_index_j[k]])
+                {
+                    buf = Array[k, k];
+                    Array[k, k] = Array[Array_index_i[k], Array_index_j[k]];
+                    Array[Array_index_i[k], Array_index_j[k]] = buf;
+                }
+                /*Console.WriteLine("Значение конечного массива:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(Array[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                //Array_max[k] = Array[Array_index_i[k], Array_index_j[k]];
+                //Array[Array_index_i[k], Array_index_j[k]] = min;*/
+            }
+
+            /*for (int k = 0; k < n; k++)
+            {
+                Array[Array_index_i[k], Array_index_j[k]] = Array[k,k];
+                Array[k, k] = Array_max[k];
+                Console.WriteLine("Значение конечного массива:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(Array[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }*/
+
+            Console.WriteLine("Значение конечного массива:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(Array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            /*Console.WriteLine("Значение начального массива:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Array_index_i[i] + " " + Array_index_j[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Значение начального массива:");
+            for (int i = 0; i < n ; i++)
+            {
+                Console.Write(Array_max[i] + " ");
+            }*/
+        }
+    }
+}
